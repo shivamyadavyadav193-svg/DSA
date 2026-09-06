1333b3fbf534bc958701f2b49ef3adcab0735d1b
SHIVAM YADAV <shivamyadavyadav193@gmail.com>
2026-09-06 23:58:33 +0530

    Time: 2 ms (77.94%), Space: 10 MB (86.39%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            int a = value(s[i]);
+
+            if(i + 1 < s.size() && a < value(s[i + 1]))
+                ans -= a;
+            else
+                ans += a;
+        }
+
+        return ans;
+    }
+
+    int value(char c) {
+        if(c == 'I') return 1;
+        if(c == 'V') return 5;
+        if(c == 'X') return 10;
+        if(c == 'L') return 50;
+        if(c == 'C') return 100;
+        if(c == 'D') return 500;
+        return 1000;
+    }
+};
